8d543b9331140898b366eab71cfdfb00d997e688
Ahmad Muzaffar B <150901060+muzibm@users.noreply.github.com>
2024-07-01 11:33:38 +0800

    Update Offload Transactional Data from DB2 to Ceph S3 Bucket.sql
    Update code

--- watsonx.data/Offload Transactional Data from DB2 to Ceph S3 Bucket.sql
+++ watsonx.data/Offload Transactional Data from DB2 to Ceph S3 Bucket.sql
@@ -1,22 +1,25 @@
 -- Hands-on: Optimizing and Scaling On-Premise Data Engineering for BI Dashboards/Reports
 -- Task 1: Offload Transactional Data from DB2 to Ceph S3 Bucket
 
+
 -- Sales Channel 1: Store Sales
-CREATE TABLE "ceph_retail"."sales"."store_sales" AS
+CREATE TABLE "ceph_retail"."sales"."store_sales_YOUR_NAME" AS
 SELECT
   *
 FROM
   "db2_retail"."sales"."store_sales";
 
+
 -- Sales Channel 2: Catalog Sales
-CREATE TABLE "ceph_retail"."sales"."catalog_sales" AS
+CREATE TABLE "ceph_retail"."sales"."catalog_sales_YOUR_NAME" AS
 SELECT
   *
 FROM
   "db2_retail"."sales"."catalog_sales";
 
+
 -- Sales Channel 3: Web Sales
-CREATE TABLE "ceph_retail"."sales"."web_sales" AS
+CREATE TABLE "ceph_retail"."sales"."web_sales_YOUR_NAME" AS
 SELECT
   *
 FROM
